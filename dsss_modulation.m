59e47e2b4a206e8f54b24728e83a6adb7db60f26
adream307 <adream307@gmail.com>
2012-09-01 19:51:01 +0800

    fix bug for bpsk

--- dsss_modulation.m
+++ dsss_modulation.m
@@ -1,4 +1,4 @@
-function [sig,carry]=dsss_modulation(primdf,init_states,sample_fs,carry_fs,chip_bps,src_bps,src)
+function [sig,msg]=dsss_modulation(primdf,init_states,sample_fs,carry_fs,chip_bps,src_bps,src)
 % Direct-sequence spread spectrum test with matlab
 % input:
 %      primdf      -- primitive polynomials for a Galois field
@@ -10,7 +10,7 @@ function [sig,carry]=dsss_modulation(primdf,init_states,sample_fs,carry_fs,chip_
 %      src         -- the information which will be modulated
 % output:
 %      sig         -- the output signal which has been modulated
-%       carry      -- the sequence the pn code xor with the source
+%      msg         -- the message which modulated into the carrier
 x=zeros(size(init_states));
 x(length(x))=1;
 chip_len=chip_bps/src_bps*length(src);
@@ -22,43 +22,6 @@ h=commsrc.pn('GenPoly',primdf,...
 s=generate(h)';
 x=repmat(src,chip_bps/src_bps,1);
 x=reshape(x,1,length(s));
-s=bitxor(s,x);
-carry=-1*ones(1,chip_len/chip_bps*carry_fs);
-times=carry_fs/chip_bps;
-remain=0;
-n=1;
-for k=s
-    remain=remain+times;
-    cur=round(remain);
-    carry(n:n+cur-1)=k*ones(1,cur);
-    remain=remain-cur;
-    n=n+cur;
-end
-sig=zeros(1,chip_len/chip_bps*sample_fs);
-carry_index=1;
-sig_index=1;
-remain_t=0;
-period_t=1/sample_fs;
-while carry_index<length(carry)
-    num=0;
-    val=carry(carry_index);
-    while carry_index<length(carry)
-        if carry(carry_index)~=val
-            break;
-        else
-            carry_index=carry_index+1;
-            num=num+1;
-        end
-    end
-    cur_t=num*period_t+remain_t;
-    cur_period=round(cur_t/period_t);
-    t=0:1/sample_fs:cur_period/carry_fs;
-    t=t(1:length(t)-1);
-    if val==1
-        sig(sig_index:sig_index+length(t)-1)=cos(2*pi*carry_fs*t);
-    else
-        sig(sig_index:sig_index+length(t)-1)=cos(2*pi*carry_fs*t+pi);
-    end
-    sig_index=sig_index+length(t);
-    remain_t=cur_t-cur_period*period_t;
-end
+msg=bitxor(s,x);
+sig=bpsk_modulatlion(sample_fs,carry_fs,chip_bps,msg);
+    
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
 Direct-sequence spread spectrum test with matlab
 usge:
-    [s,c]dsss_modulation(fliplr(gfprimdf(10)),ones(1,10),20000,7000,1023,1,1);
+    [s,m]dsss_modulation(fliplr(gfprimdf(10)),ones(1,10),20000,7000,1023,1,1);
     showfft(s,20000)
